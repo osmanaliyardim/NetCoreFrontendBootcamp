63f7b354cdc4351b85ccc26772f197606f510bf7
Osman Ali YARDIM <7006079@bakircay.edu.tr>
2021-07-16 09:40:49 +0300

    Layout ve kategoriler navbarı oluşturuldu. Kategori bazlı listeleme uygulandı

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { empty } from 'rxjs';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  categories : Category[] = [];
+
+  currentCategory:Category;
+
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  getCategories() {
+    //Asenkron çalışmaları subscribe ile hallediyoruz
+    //Aynı anda çalışan methodlarda, uzun olan method geride kalıyor
+    //Üste yazılmasına rağmen
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories = response.data
+    })
+  }
+
+  setCurrentCategory(category:Category){
+    this.currentCategory = category;
+  }
+
+  getCurrentCategoryClass(category:Category){
+    if(category == this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
+
+  setCurrentCategoryAll(){
+    //this.currentCategory={} as Category;
+    //this.currentCategory=<Category>{};
+    this.currentCategory= {categoryId:0,categoryName:"Tüm Ürünler"};
+  }
+
+  getAllCategoriesClass(){
+    if(this.currentCategory.categoryName == "Tüm Ürünler"){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { ProductService } from 'src/app/services/product.service';
 
 //axios, fetch -> React'ta httpclient için kullanılanlar
@@ -15,15 +15,32 @@ export class ProductComponent implements OnInit {
   products:Product[] = [];
   dataLoaded = false;
 
-  constructor(private productService:ProductService) { }
+  constructor(private productService:ProductService,
+    private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }
+      else{
+        this.getProducts()
+      }
+    })
   }
 
   getProducts() {
     //Asenkron çalışmaları subscribe ile hallediyoruz
+    //Aynı anda çalışan methodlarda, uzun olan method geride kalıyor
+    //Üste yazılmasına rağmen
     this.productService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })
+  }
+
+  getProductsByCategory(categoryId:number) {
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
       this.products = response.data
       this.dataLoaded = true;
     })
